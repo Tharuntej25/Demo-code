a3249b0a44b0e3ef1d45844ebb5dcb2a3bea6013
Tharuntej25 <164775985+Tharuntej25@users.noreply.github.com>
2024-03-25 14:17:30 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,14 @@
+public class Student
+{
+    public static void main (String []args)
+    {
+        int studentId = 532684;
+        String studentName = "Tharun";
+        int Marks = 85;
+        double Percentage = 92.3;
+        System.out.println("StudentId; " + studentId); 
+        System.out.println("StudentName; " + studentName); 
+        System.out.println("Marks; " + Marks); 
+            System.out.println("Percentage; " + Percentage); 
+  }
+}
