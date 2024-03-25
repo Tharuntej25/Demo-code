c62de9d086328b77bfc87aade524b72bbc29244f
Tharuntej25 <164775985+Tharuntej25@users.noreply.github.com>
2024-03-25 14:03:55 +0530

    Add files via upload

---
+++ Dog.java
@@ -0,0 +1,12 @@
+public class Dog
+ {
+    public static void main (String []args){
+    String breed = "German Shpeard";
+    int height = 30;
+    int weight = 35;
+    System.out.println("breed: "+breed);
+     System.out.println("Height: "+ height);
+     System.out.println("Weight: "+ weight);
+
+ }
+ }
