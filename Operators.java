2fc5cebbe962a1fdc930bc2bc669620c57624fc6
Tharuntej25 <164775985+Tharuntej25@users.noreply.github.com>
2024-03-26 13:34:05 +0530

    Add files via upload

---
+++ Operators.java
@@ -0,0 +1,13 @@
+public class Operators
+{
+    public static void main(String[] args)
+    {
+        int i = 5;
+        int j = 8;
+        System.out.println(i+j);
+        System.out.println(i-j);
+        System.out.println(i*j);
+        System.out.println(i/j);
+        System.out.println(i%j);
+    }
+}
---
+++ Unary.java
@@ -0,0 +1,27 @@
+public class Unary
+{
+    public static void main(String[] args)
+    {
+        int i = 9;
+        int j = 4;
+
+       // System.out.println(i);
+         //System.out.println(--i);
+          //System.out.println(i--);
+           //System.out.println(i);
+
+            //System.out.println(i==j);     
+             //System.out.println(i!=j);
+              //System.out.println(i<j);
+               //System.out.println(i>j);
+                //System.out.println(i<=j);
+                 //System.out.println(i>=j);
+
+                 //System.out.println(i<j || i==j);
+                  //System.out.println(i<j && i==j);
+
+i +=5;
+System.out.println(i);
+
+    }
+}
