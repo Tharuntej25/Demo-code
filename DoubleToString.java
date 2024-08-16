826b871d89839c555ea2d703f9ce203d1dfa752d
Tharuntej25 <164775985+Tharuntej25@users.noreply.github.com>
2024-08-16 13:21:36 +0530

    Add files via upload

---
+++ DoubleToString.java
@@ -0,0 +1,8 @@
+public class DoubleToString {
+    public static void main(String[] args) {
+        double d = 123.45;
+        String s = Double.toString(d);
+        System.out.println("Double value: " + d);
+        System.out.println("String value: " + s);
+    }
+}
